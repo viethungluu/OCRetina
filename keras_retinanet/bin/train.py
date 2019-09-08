41ccc20d8995709323c8f42657699dd8d0234edc
Hung Luu <hunglv@fimo.edu.vn>
2019-09-08 13:05:24 +0900

    bug fix

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -171,7 +171,7 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
         checkpoint = keras.callbacks.ModelCheckpoint(
             os.path.join(
                 args.snapshot_path,
-                '{backbone}_{dataset_type}_{{epoch:02d}}.h5'.format(backbone=args.backbone, dataset_type=args.dataset_type)
+                '{backbone}_{{epoch:02d}}.h5'.format(backbone=args.backbone)
             ),
             verbose=1,
             # save_best_only=True,
