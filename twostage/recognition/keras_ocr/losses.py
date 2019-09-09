50fc4098f44e6a7119d266f7932e44bc8baed54b
Hung Luu <hunglv@fimo.edu.vn>
2019-09-10 06:00:06 +0900

    debug ctc loss

--- twostage/recognition/keras_ocr/losses.py
+++ twostage/recognition/keras_ocr/losses.py
@@ -16,7 +16,7 @@ def ctc():
 		
 		y_pred 			= y_pred[:, 2:, :] # batch x time_step x num_categories
 
-		print(K.int_shape(y_pred), K.int_shape(labels), K.int_shape(input_length), K.int_shape(label_length))
+		print(K.int_shape(y_pred), K.int_shape(y_true))
 
 		return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
