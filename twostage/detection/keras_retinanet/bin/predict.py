054a7390e88de30523f0d97073b19acce2b3c0af
Hung Luu <hunglv@fimo.edu.vn>
2019-09-10 09:18:45 +0900

    update readme

--- twostage/detection/keras_retinanet/bin/predict.py
+++ twostage/detection/keras_retinanet/bin/predict.py
@@ -122,7 +122,7 @@ class RetinaNetWrapper(object):
         for label in range(self.num_classes):
             all_detections[label] = image_detections[image_detections[:, -1] == label, :-1]
 
-        return all_detections
+        return np.array(all_detections)
 
 def parse_args(args):
     """ Parse the arguments.
@@ -176,6 +176,7 @@ def main(args=None):
     image = cv2.imread(args.image_path)
     all_detections = model.predict(image, args.save_path)
 
+    print(all_detections.shape)
     all_detections.dump("detections.dat")
 
 if __name__ == '__main__':
